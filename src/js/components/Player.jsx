6fa4a3814bd74af8e0002545a646f9cc1b8cbf32
Jakub Bem <bem.biznes@gmail.com>
2018-05-19 19:52:04 +0200

    Working player states

--- src/js/components/Player.jsx
+++ src/js/components/Player.jsx
@@ -3,19 +3,32 @@ import React from 'react';
 export default class Player extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            id : this.props.id,
+            score: 0,
+            currentCard: "",
+            history: [],
+
+        }
     }
     
     getCard = e => {
-       const h = this.props.getNextCard();
-       console.log(h);
-       const value = this.props.cardValues[`${h}`];
-       console.log(value);
+       const card = this.props.getNextCard();
+       this.setState({currentCard : card});
+
+       const history = [...this.state.history];
+       history.push(card);
+       this.setState({history});
+
+       const value = this.props.cardValues[`${card}`];
+       this.setState({score : this.state.score + value});
     };
 
     render(){
         return (
             <div className="player">
                 <button onClick={this.getCard}>PLAYER</button>
+                <h1>{this.state.currentCard}</h1>
             </div>
         );
     }
